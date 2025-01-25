d1ed9bfc869dffdea0b14a7acfc0e9a225d96928
Abonsama <aboon20018@gmail.com>
2025-01-25 23:48:42 +0200

    Fix:set the functionality of free market items page

--- app/collectionMarketPage/page.tsx
+++ app/collectionMarketPage/page.tsx
@@ -1,17 +1,68 @@
 import Link from "next/link"
+import Header from "../header"
+import Navbar from "../navbar"
 export default function CollectionPage(){
+    function getData(){
+        let fetchData=[
+            {
+                price:200,
+                name:"xxx",
+                describtion:" lajsfj sldfj s  sofjs sj sdjkf ssj sfs"
+            },
+            {
+                price:200,
+                name:"yyy",
+                describtion:" lajsfj sldfj s  sofjs sj sdjkf ssj sfs"
+            },
+            {
+                price:200,
+                name:"zzz",
+                describtion:" lajsfj sldfj s  sofjs sj sdjkf ssj sfs"
+            },
+            {
+                price:200,
+                name:"ttt",
+                describtion:" lajsfj sldfj s  sofjs sj sdjkf ssj sfs"
+            },
+            {
+                price:200,
+                name:"bbb",
+                describtion:" lajsfj sldfj s  sofjs sj sdjkf ssj sfs"
+            },
+            {
+                price:200,
+                name:"sss",
+                describtion:" lajsfj sldfj s  sofjs sj sdjkf ssj sfs"
+            },
+            {
+                price:200,
+                name:"ooo",
+                describtion:" lajsfj sldfj s  sofjs sj sdjkf ssj sfs"
+            },
+        ]
+            return (
+                <ul>
+                    {fetchData.map((i) => (
+                    <li key={`${i.name}`}>
+                        <span>{i.name}</span>
+                        <span>{i.describtion}</span>
+                        <span>{i.price}</span>
+                    </li>
+                    ))}
+                    </ul>
+                );
+            }
     return(
         <section>
+            <Header></Header>
             <button className="filterButton"></button>
             <Link href={"itemslist"}></Link>
             <div className="container">
             <ul>
-                <li>item</li>
-                <li>item</li>
-                <li>item</li>
-                <li>item</li>
+                {getData()}
             </ul>
             </div>
+            <Navbar></Navbar>
         </section>
     )
 }
