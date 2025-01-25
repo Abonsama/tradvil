a0b068802143f75d5c87e02ab34fd768003a7581
Abonsama <aboon20018@gmail.com>
2025-01-25 23:21:19 +0200

    Fix:set the functionality of crypto page

--- app/cryptoPage/page.tsx
+++ app/cryptoPage/page.tsx
@@ -1,17 +1,58 @@
-import Link from "next/link"
+import Header from "../header"
+import Navbar from "../navbar"
 export default function CryptoPage(){
+    function getData(){
+        let fetchData=[
+            {
+                price:200,
+                name:"xxx"
+            },
+            {
+                price:200,
+                name:"yyy"
+            },
+            {
+                price:200,
+                name:"zzz"
+            },
+            {
+                price:200,
+                name:"ttt"
+            },
+            {
+                price:200,
+                name:"bbb"
+            },
+            {
+                price:200,
+                name:"sss"
+            },
+            {
+                price:200,
+                name:"ooo"
+            },
+        ]
+            return (
+                <ul>
+                    {fetchData.map((i) => (
+                    <li key={`${i.name}`}>
+                        <span>{i.name}</span>
+                        <span>{i.price}</span>
+                    </li>
+                    ))}
+                    </ul>
+                );
+    }
     return(
         <section>
+            <Header></Header>
             <button className="filterButton"></button>
-            <Link href={"itemslist"}></Link>
             <div className="container">
             <ul>
-                <li>item</li>
-                <li>item</li>
-                <li>item</li>
-                <li>item</li>
+                {getData()}
             </ul>
             </div>
+            <Navbar></Navbar>
         </section>
     )
 }
