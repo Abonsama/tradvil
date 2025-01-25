c9198dd3b6184bfe1775ebcb779bdaede704c05c
Abonsama <aboon20018@gmail.com>
2025-01-26 00:19:36 +0200

    Fix:set the functionality of social button

--- app/messages.tsx
+++ app/messages.tsx
@@ -1,7 +1,60 @@
 
 export default function MessagesButton(){
+    function getData(){
+        let fetchData=[
+            {
+                name:"xxx",
+                lastMessage:" lajsfj sldfj s  sofjs sj sdjkf ssj sfs"
+            },
+            {
+                name:"yyy",
+                lastMessage:" lajsfj sldfj s  sofjs sj sdjkf ssj sfs"
+            },
+            {
+                name:"zzz",
+                lastMessage:" lajsfj sldfj s  sofjs sj sdjkf ssj sfs"
+            },
+            {
+                name:"www",
+                lastMessage:" lajsfj sldfj s  sofjs sj sdjkf ssj sfs"
+            },
+            {
+                name:"eee",
+                lastMessage:" lajsfj sldfj s  sofjs sj sdjkf ssj sfs"
+            },
+            {
+                name:"qqq",
+                lastMessage:" lajsfj sldfj s  sofjs sj sdjkf ssj sfs"
+            },
+            {
+                name:"nnn",
+                lastMessage:" lajsfj sldfj s  sofjs sj sdjkf ssj sfs"
+            },
+            {
+                name:"hhh",
+                lastMessage:" lajsfj sldfj s  sofjs sj sdjkf ssj sfs"
+            },
+            {
+                name:"ddd",
+                lastMessage:" lajsfj sldfj s  sofjs sj sdjkf ssj sfs"
+            },
+        ]
+            return (
+                <ul>
+                    {fetchData.map((i) => (
+                    <li key={`${i.name}`}><button>
+                        <span>{i.name}</span>
+                        <span>{i.lastMessage}</span>
+                    </button>
+                    </li>
+                    ))}
+                    </ul>
+                );
+            }
     return(
-        <aside></aside>
+        <aside className="social">
+            {getData()}
+        </aside>
     )
 }
 /*
